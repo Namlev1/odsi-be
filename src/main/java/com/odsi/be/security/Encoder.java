66c64b05cecaecdc08897de6e99468ce53f5f82d
namlev <mikipatkowski@protonmail.com>
2025-01-20 17:16:03 +0100

    Change BCrypt to Argon2

--- src/main/java/com/odsi/be/security/Encoder.java
+++ src/main/java/com/odsi/be/security/Encoder.java
@@ -1,15 +1,34 @@
 package com.odsi.be.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
-@Configuration
-public class Encoder {
+@Service
+public class Encoder implements PasswordEncoder {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10); // 10 key stretch
+    private final PasswordEncoder encoder;
+    @Value("${security.pepper:secret-value}")
+    private String PEPPER;
+
+    public Encoder() {
+        this.encoder = new Argon2PasswordEncoder(
+                16, // Memory in KB (e.g., 16 * 1024 for 16 MB)
+                32, // Salt length
+                1,  // Parallelism (threads)
+                32, // Hash length
+                3   // Iterations (key stretching)
+        );
+    }
+
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return encoder.encode(rawPassword + PEPPER);
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return encoder.matches(rawPassword + PEPPER, encodedPassword);
     }
 }
